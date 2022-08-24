fbeeccc2393cb83bedee1db18dedcfc9ed2bdd59
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-08-24 20:05:10 +0400

    Update mint.js

--- src/utils/mint.js
+++ src/utils/mint.js
@@ -1,7 +1,7 @@
 import {create as ipfsHttpClient} from "ipfs-http-client";
 import axios from "axios";
-import BigNumber  from "bignumber.js";
-import { createLot } from "./parking-space"; 
+import BigNumber from "bignumber.js";
+import { createLot } from "./parking-space";
 // initialize IPFS
 
 const auth =
@@ -21,36 +21,39 @@ const client = ipfsHttpClient({
     authorization: auth,
   }
 })
-
 // mint an NFT
 export const createNft = async (
 	minterContract,
 	performActions,
-	location, description, ipfsImage
+	location,
+	description,
+	ipfsImage
 ) => {
 	await performActions(async (kit) => {
 		if (!location || !description || !ipfsImage) return;
-		const {defaultAccount} = kit;
+		const { defaultAccount } = kit;
 
 		// convert NFT metadata to JSON format
 		const data = JSON.stringify({
 			location,
 			description,
 			image: ipfsImage,
-			owner: defaultAccount
+			owner: defaultAccount,
 		});
 
 		try {
-
 			// save NFT metadata to IPFS
 			const added = await client.add(data);
 
 			// IPFS url for uploaded metadata
 			const url = `https://ipfs.infura.io/ipfs/${added.path}`;
 
-
 			// mint the NFT and save the IPFS url to the blockchain
-			let transaction = await createLot(minterContract, performActions, url);
+			let transaction = await createLot(
+				minterContract,
+				performActions,
+				url
+			);
 			return transaction;
 		} catch (error) {
 			console.log("Error uploading file: ", error);
@@ -58,27 +61,25 @@ export const createNft = async (
 	});
 };
 
-
 // function to upload a file to IPFS
 export const uploadToIpfs = async (e) => {
 	const file = e.target.files[0];
 	if (!file) return;
 	try {
-		const added = await client.add(file, {
-			progress: (prog) => console.log(`received: ${prog}`),
-		});
+		const added = await client.add(file);
 		return `https://ipfs.infura.io/ipfs/${added.path}`;
 	} catch (error) {
 		console.log("Error uploading file: ", error);
 	}
 };
 
-
 // fetch all NFTs on the smart contract
 export const getNfts = async (minterContract) => {
 	try {
 		const nfts = [];
-		const nftsLength = await minterContract.methods.getParkingLotsLength().call();
+		const nftsLength = await minterContract.methods
+			.getParkingLotsLength()
+			.call();
 		for (let i = 0; i < Number(nftsLength); i++) {
 			const nft = new Promise(async (resolve) => {
 				const res = await minterContract.methods.tokenURI(i).call();
@@ -96,14 +97,14 @@ export const getNfts = async (minterContract) => {
 					status: Number(lot[7]),
 					location: meta.data.location,
 					image: meta.data.image,
-					description: meta.data.description
+					description: meta.data.description,
 				});
 			});
 			nfts.push(nft);
 		}
 		return Promise.all(nfts);
 	} catch (e) {
-		console.log({e});
+		console.log({ e });
 	}
 };
 
@@ -114,17 +115,16 @@ export const fetchNftMeta = async (ipfsUrl) => {
 		const meta = await axios.get(ipfsUrl);
 		return meta;
 	} catch (e) {
-		console.log({e});
+		console.log({ e });
 	}
 };
-
 
 // get the owner address of an NFT
 export const fetchLot = async (minterContract, index) => {
 	try {
 		return await minterContract.methods.getLot(index).call();
 	} catch (e) {
-		console.log({e});
+		console.log({ e });
 	}
 };
 
@@ -134,6 +134,6 @@ export const fetchNftContractOwner = async (minterContract) => {
 		let owner = await minterContract.methods.owner().call();
 		return owner;
 	} catch (e) {
-		console.log({e});
+		console.log({ e });
 	}
 };
